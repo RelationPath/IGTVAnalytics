91a3b4472079f75ba40d311ab1d7f7c4f15d73d3
BraytonK <brayton.k14@gnail.com>
2018-07-15 23:03:44 +0000

    returned to using Python and added code to pull remote image and extract text

--- igtvTextExtract.py
+++ igtvTextExtract.py
@@ -1,1 +1,23 @@
-hello
+import argparse 
+from enum import Enum
+import io
+
+from google.cloud import vision
+from google.cloud.vision import types
+from PIL imprt Image, ImageDraw
+
+class FeatureType(Enum):
+   PAGE = 1
+   BLOCK = 2
+   PARA = 3
+   WORD = 4
+   SYMBOL = 5
+
+
+def draw_boxes(image, bounds, color):
+   """Draw a border around tehe image using what is given in the vector list assinged above"""
+   draw = ImageDraw.Draw(image)
+
+   for bound in bounds:
+      draw.polygon
+   """Unfinished!"""
