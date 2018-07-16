0a121309f623730e6c1140643bd8d4c9dacbf0a6
BraytonK <brayton.k14@gnail.com>
2018-07-16 20:33:36 +0000

    set this to be the true file and deleted the mess ups and added commands and parses to the script

--- igtvTextExtract.py
+++ igtvTextExtract.py
@@ -1,23 +1,45 @@
-import argparse 
-from enum import Enum
+import argparse
 import io
+import re
 
+from google.cloud import storage
 from google.cloud import vision
-from google.cloud.vision import types
-from PIL imprt Image, ImageDraw
+from google.protobuf import json_format
 
-class FeatureType(Enum):
-   PAGE = 1
-   BLOCK = 2
-   PARA = 3
-   WORD = 4
-   SYMBOL = 5
+inputUri = 'https://storage.googleapis.com/igtv-input-data/Sample%20Data/Screenshot_20180713-153744.png'
 
+def detect_text_uri(uri):
+   """Collects input when user calls on program to detect text from teh command line and saves the uri given after the call as the variable to be called on in the program"""
 
-def draw_boxes(image, bounds, color):
-   """Draw a border around tehe image using what is given in the vector list assinged above"""
-   draw = ImageDraw.Draw(image)
+   client = vision.ImageAnnotatorClient()
+   image = types.Image()
+   image.source.image_uri = uri
+   """defines the uri to be called on as what the user defined it to be in the command call"""
 
-   for bound in bounds:
-      draw.polygon
-   """Unfinished!"""
+   response = client.text_detection(image=image)
+   texts = response.text_annotations
+   print('Texts:')
+
+   for text in texts:
+      print('\n"{}"'.format(text.description))
+
+   vertices = (['({},{})'.format(vertex.x, vertex.y)
+         for vertex in text.bounding_poly.vertices])
+
+   print('bounds: {}'.format(','.join(vertices)))
+
+print(detect_text_uri(inputUri))
+
+def run_uri(args):
+    if args.command == 'text-uri':
+        detect_text_uri(args.uri)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(dest='command')
+
+    text_file_parser = subparsers.add_parser(
+        'text-uri', help=detect_text_uri.__doc__)
+    text_file_parser.add_argument('uri')
